397fd03147011b8517c8b26c2c6fb2a7e9b388c4
Md Jakaria <jakaria.dcc11@gmail.com>
2025-02-04 20:16:16 -0500

    original rand.py has 3 low vulnarability

--- rand.py
+++ rand.py
@@ -1,13 +1,7 @@
-import secrets
-# import subprocess
+import subprocess
 
 def random_array(arr):
     for i in range(len(arr)):
-        # shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        # arr[i] = int(shuffled_num.stdout)
-        # Utilizing Python's built-in secrets module to 
-        # generate cryptographically secure random numbers
-        # Generate a random number between 1 and 20
-        shuffled_num = secrets.randbelow(20) + 1  
-        arr[i] = shuffled_num
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+        arr[i] = int(shuffled_num.stdout)
     return arr
